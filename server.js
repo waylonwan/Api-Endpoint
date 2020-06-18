81b63b4abe95ccd1af1c640ff2ed2c4810d2e281
waylonwan <44353914+waylonwan@users.noreply.github.com>
2020-06-18 15:33:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -33,17 +33,17 @@ app.get('/api-endpoint', function(request, response) {
 	email: "stanley.ng@hkt.com",
 	balance: 6505,
 	expBalance: [{balance: 2000, expDate: "31/03/2021"}, {balance: 2505, expDate: "30/09/2021"}, {balance: 2000, expDate: "31/12/2021"}],
-	transaction: [{transName: "(Bill: 4/2020)", transDesc: "Club SIM - Bill: 8048903747", transType: "Earn", transBalance: 2, transDate: "23/05/2020"},
-				  {transName: "(Bill: 4/2020)", transDesc: "1O1O - Bill: 75100213338565", transType: "Earn", transBalance: 35, transDate: "23/05/2020"},
-				  {transName: "1O1O Service Plan Renewal Offer", transType: "Earn", transBalance: 1500, transDate: "22/05/2020"},
-				  {transName: "Club Like Purchase", transDesc: "order no:3000038875", transType: "Earn", transBalance: 1500, transDate: "08/05/2020"},
-				  {transName: "(Bill: 3/2020)", transDesc: "1O1O - Bill: 75100213338565", transType: "Earn", transBalance: 35, transDate: "23/04/2020"},
-				  {transName: "(Bill: 2/2020)", transDesc: "1O1O - Bill: 75100213338565", transType: "Earn", transBalance: 38, transDate: "23/03/2020"},
-				  {transName: "Club SIM - Service Pack", transType: "Burn", transBalance: 510, transDate: "14/01/2020"},
-				  {transName: "broadway circuit Ticket Exchange Coupon (1pc) (Quantity: 1)", transType: "Burn", transBalance: 500, transDate: "27/09/2019"},
-				  {transName: "Club SIM - Service Pack", transType: "Burn", transBalance: 75, transDate: "30/06/2019"},
-				  {transName: "Club SIM - Service Pack", transType: "Burn", transBalance: 75, transDate: "30/05/2019"}]
-    };
+	transaction: [{transId: 0, transName: "(Bill: 4/2020)", transDesc: "Club SIM - Bill: 8048903747", transType: "Earn", transBalance: 2, transDate: "23/05/2020"},
+				  {transId: 1, transName: "(Bill: 4/2020)", transDesc: "1O1O - Bill: 75100213338565", transType: "Earn", transBalance: 35, transDate: "23/05/2020"},
+				  {transId: 2, transName: "1O1O Service Plan Renewal Offer", transType: "Earn", transBalance: 1500, transDate: "22/05/2020"},
+				  {transId: 3, transName: "Club Like Purchase", transDesc: "order no:3000038875", transType: "Earn", transBalance: 1500, transDate: "08/05/2020"},
+				  {transId: 4, transName: "(Bill: 3/2020)", transDesc: "1O1O - Bill: 75100213338565", transType: "Earn", transBalance: 35, transDate: "23/04/2020"},
+				  {transId: 5, transName: "(Bill: 2/2020)", transDesc: "1O1O - Bill: 75100213338565", transType: "Earn", transBalance: 38, transDate: "23/03/2020"},
+				  {transId: 6, transName: "Club SIM - Service Pack", transType: "Burn", transBalance: 510, transDate: "14/01/2020"},
+				  {transId: 7, transName: "broadway circuit Ticket Exchange Coupon (1pc) (Quantity: 1)", transType: "Burn", transBalance: 500, transDate: "27/09/2019"},
+				  {transId: 8, transName: "Club SIM - Service Pack", transType: "Burn", transBalance: 75, transDate: "30/06/2019"},
+				  {transId: 9, transName: "Club SIM - Service Pack", transType: "Burn", transBalance: 75, transDate: "30/05/2019"}]
+};
     
     response.setHeader('Access-Control-Allow-Origin','*');
     response.send(JSON.parse(JSON.stringify(jsonContent)));
